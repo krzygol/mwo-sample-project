f32e23bb9e5c0583dc9b4e9ff33a6f14d69ef630
Krzysztof Golebiowski <krzygol@gmail.com>
2026-05-31 12:18:14 +0200

    message

--- src/main/java/pl/edu/agh/mwo/FileLoader.java
+++ src/main/java/pl/edu/agh/mwo/FileLoader.java
@@ -1,4 +1,35 @@
 package pl.edu.agh.mwo;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileLoader {
+
+    public static List<Pitstop> loadPitstops(String fileName) throws IOException {
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(new File(fileName));
+
+        List<Pitstop> pitstops = new ArrayList<>();
+
+        for (JsonNode node : root) {
+            Pitstop pitstop = new Pitstop();
+
+            pitstop.setSessionKey(node.get("session_key").asInt());
+            pitstop.setMeetingKey(node.get("meeting_key").asInt());
+            pitstop.setDate(node.get("date").asText());
+            pitstop.setDriverNumber(node.get("driver_number").asInt());
+            pitstop.setPitDuration(node.get("pit_duration").asDouble());
+            pitstop.setLapNumber(node.get("lap_number").asInt());
+
+            pitstops.add(pitstop);
+        }
+
+        return pitstops;
+    }
 }
